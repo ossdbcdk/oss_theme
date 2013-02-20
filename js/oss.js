8b40bc875a5661756ce0a8d2968213b3847bba8f
Anna Caraman <ac@inleadmedia.com>
2013-02-20 15:27:57 +0000

    case 2235 - OSS styles.

--- js/oss.js
+++ js/oss.js
@@ -1,6 +1,8 @@
 (function ($) {
   $(document).ready(function() {
     $('.mailchimp-newsletter-wrapper input[type=text]').attr('placeholder', $('.mailchimp-newsletter-wrapper label').text());
-    //console.log($('.mailchimp-newsletter-wrapper label').text());
+    if ($('.pane-menu-menu-blog-kategorier').children().length > 0) {
+      $('body').addClass('blog-category');
+    }
   });  
 })(jQuery);
