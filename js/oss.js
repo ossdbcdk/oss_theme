630cafdf83c558f4cb9bf6792f95eec1e3ce3a61
Anna Caraman <ac@inleadmedia.com>
2013-02-22 10:58:36 +0000

    case 2235 - OSS blog styles.

--- js/oss.js
+++ js/oss.js
@@ -1,8 +1,9 @@
 (function ($) {
   $(document).ready(function() {
     $('.mailchimp-newsletter-wrapper input[type=text]').attr('placeholder', $('.mailchimp-newsletter-wrapper label').text());
-    if ($('.pane-menu-menu-blog-kategorier').children().length > 0) {
+    if ($('#block-menu-menu-blog').children().length > 0) {
       $('body').addClass('blog-category');
     }
+    $('.blog-category .navbar-fixed-top .container').prepend('<a href="/blog" class="header-blog-link">Blog</a>');
   });  
 })(jQuery);
